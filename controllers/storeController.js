2bbc6f2cceffe7f4ad9f38909cb5fbd718fb8121
mwidiy <muhammadwidi72@gmail.com>
2026-06-23 15:05:39 +0700

    penambahan fitur direc wa untuk obrolan

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -48,7 +48,7 @@ const getStore = async (req, res) => {
 // Update Info
 const updateStore = async (req, res) => {
     try {
-        let { name, isOpen, bankName, bankNumber, bankHolder, ewalletType, ewalletNumber, ewalletName, whatsappNumber, isKasirQrVerificationEnabled, cashPaymentMode, isCashActive, isDineInActive, isTakeawayActive, isDeliveryActive, isAiEnabled, isAutoReplyEnabled } = req.body;
+        let { name, isOpen, bankName, bankNumber, bankHolder, ewalletType, ewalletNumber, ewalletName, whatsappNumber, isKasirQrVerificationEnabled, cashPaymentMode, isCashActive, isDineInActive, isTakeawayActive, isDeliveryActive, isWaOrderNotificationActive, isAiEnabled, isAutoReplyEnabled } = req.body;
         if (!req.storeId) return res.status(400).json({ error: 'User tidak memiliki akses Toko' });
 
         // --- HARDENING: SERVER-SIDE VALIDATION & SANITIZATION ---
@@ -112,6 +112,7 @@ const updateStore = async (req, res) => {
             isDineInActive: (isDineInActive === 'true' || isDineInActive === true) ? true : (isDineInActive === 'false' || isDineInActive === false ? false : undefined),
             isTakeawayActive: (isTakeawayActive === 'true' || isTakeawayActive === true) ? true : (isTakeawayActive === 'false' || isTakeawayActive === false ? false : undefined),
             isDeliveryActive: (isDeliveryActive === 'true' || isDeliveryActive === true) ? true : (isDeliveryActive === 'false' || isDeliveryActive === false ? false : undefined),
+            isWaOrderNotificationActive: (isWaOrderNotificationActive === 'true' || isWaOrderNotificationActive === true) ? true : (isWaOrderNotificationActive === 'false' || isWaOrderNotificationActive === false ? false : undefined),
             isAiEnabled: (isAiEnabled === 'true' || isAiEnabled === true) ? true : (isAiEnabled === 'false' || isAiEnabled === false ? false : undefined),
             isAutoReplyEnabled: (isAutoReplyEnabled === 'true' || isAutoReplyEnabled === true) ? true : (isAutoReplyEnabled === 'false' || isAutoReplyEnabled === false ? false : undefined)
         };
